f1708d165a6156fce280dd8a246e7226634d21a6
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2020-11-09 15:55:10 +0100

    admin and first view

--- woodworks/views.py
+++ woodworks/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Woodwork
+
 def index(request):
-  return HttpResponse("Hello, world. You're at the woodworks index.")
+  woodworks = Woodwork.objects.order_by('created_at')
+  return render(request, 'index.html', {'woodworks': woodworks})
+
+
+def detail(request, woodwork_id):
+    return HttpResponse("You're looking at woodwork %s." % woodwork_id)
