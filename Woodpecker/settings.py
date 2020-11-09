042de704f4ae624d75bf7c43c83ac5900c267285
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2020-11-09 14:36:02 +0100

    initials migrations

--- Woodpecker/settings.py
+++ Woodpecker/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'woodworks.apps.WoodworksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- woodworks/models.py
+++ woodworks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Woodwork(models.Model):
+  title = models.TextField()
+  description = models.TextField()
+  publication_date = models.DateTimeField()
+  created_at = models.DateTimeField()
+
+  class Meta:
+    db_table = "woodworks"
+
+  def __str__(self):
+    return self.title + " - " + self.description
