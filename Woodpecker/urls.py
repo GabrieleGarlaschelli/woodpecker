d2502a7bf4e1cd6371db2b6a944cbef834ff821f
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2020-11-09 19:03:56 +0100

    static files and base.html

--- Woodpecker/urls.py
+++ Woodpecker/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('woodworks/', include('woodworks.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^$', RedirectView.as_view(url='woodworks/', permanent=False), name='index'),
 ]
