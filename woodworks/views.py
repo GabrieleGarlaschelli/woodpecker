a0ee54faa4bd2006b46783f761de809e9cfd2993
Emanuele Moia <emanuele.moia@hotmail.com>
2020-11-18 17:07:48 +0100

    add woodwork section fix about us add def in views

--- woodworks/views.py
+++ woodworks/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import Woodwork
 
 def index(request):
-  woodworks = Woodwork.objects.order_by('created_at')
+  woodworks = Woodwork.objects.order_by('-publication_date')[:3]
   return render(request, 'index.html', {'woodworks': woodworks})
 
 
