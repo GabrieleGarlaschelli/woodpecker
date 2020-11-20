c2e005e4ed4c535df355a0039a07493ac5b88c23
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2020-11-20 16:57:51 +0100

    fix admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Customer(models.Model)
+class Customer(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
+  birthday = models.DateField(null=True)
+
