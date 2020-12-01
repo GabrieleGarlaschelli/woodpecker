22e8ae126f869c9f82779b7bad2042c4f6849a74
Gabriele Garlaschelli <gabriele.garlaschelli@binarysystem.eu>
2020-12-01 13:02:39 +0100

    admin model registration

--- woodworks/admin.py
+++ woodworks/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Woodwork
-from .models import Like
+from .models import Woodwork, Like, Order
 
 admin.site.register(Woodwork)
 admin.site.register(Like)
+admin.site.register(Order)
