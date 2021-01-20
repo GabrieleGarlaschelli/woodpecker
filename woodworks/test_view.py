21867a6de6252e1bad70d16cd0b8cc5e8ad64aa9
Emanuele Moia <emanuele.moia@hotmail.com>
2021-01-20 21:33:31 +0100

    fix

--- woodworks/test_view.py
+++ woodworks/test_view.py
@@ -6,4 +6,24 @@ class TestViews(TestCase):
     def test_index(self):
         response = self.client.get(reverse('index'))
         self.assertEquals(response.status_code, 302)
-#da farne almeno 5/6
+    
+    def test_detail(self):
+        response = self.client.get(reverse('woodwork_detail'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_rate(self):
+        response = self.client.get(reverse('rate'))
+        self.assertEquals(response.status_code, 200)
+
+    # def test_order(self):
+    #     response = self.client.get(reverse('order'))
+    #     self.assertEquals(response.status_code, 200)
+
+    def test_is_liked(self):
+        response = self.client.get(reverse('is_liked'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_like(self):
+        response = self.client.get(reverse('like'))
+        self.assertEquals(response.status_code, 200)
+        
